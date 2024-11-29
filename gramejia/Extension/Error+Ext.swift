67ded036e3bf32704948f0499a32cddedf10537a
adamilham-dev <adamilham3004@gmail.com>
2024-11-29 14:37:56 +0700

    Create custom error for database core data

---
+++ gramejia/Extension/Error+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  Error+Ext.swift
+//  gramejia
+//
+//  Created by Adam on 29/11/24.
+//
+
+import Foundation
+
+enum DatabaseError: LocalizedError {
+    case dataNotFound
+    case invalidDatabase
+}
