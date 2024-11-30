3c78513c852d2f1f6ed84086a7b6df45c7683815
adamilham-dev <adamilham3004@gmail.com>
2024-11-30 23:36:20 +0700

    Create Book Mapper for domain - data layer transformation

--- gramejia/Core/Domain/BookModel.swift
+++ gramejia/Core/Domain/BookModel.swift
@@ -10,13 +10,12 @@ import Foundation
 struct BookModel: Identifiable {
     let id: String
     let author: String
-    let coverImage: String
-    let price: String
+    let coverImage: String?
+    let price: Double
     let publishedDate: String
     let publisher: String
-    let stock: String
+    let stock: Int64
     let synopsis: String
     let title: String
     let updatedDate: String
-    
 }
---
+++ gramejia/Core/Mapper/BookMapper.swift
@@ -0,0 +1,39 @@
+//
+//  BookMapper.swift
+//  gramejia
+//
+//  Created by Rivaldo Fernandes on 30/11/24.
+//
+
+import Foundation
+import CoreData
+
+final class BookMapper {
+    static func bookEntityToDomain(_ entity: BookEntity) -> BookModel {
+        return BookModel(
+            id: entity.id ?? "",
+            author: entity.author ?? "",
+            coverImage: entity.coverImage,
+            price: entity.price,
+            publishedDate: entity.publishedDate ?? "",
+            publisher: entity.publisher ?? "",
+            stock: entity.stock,
+            synopsis: entity.synopsis ?? "",
+            title: entity.title ?? "",
+            updatedDate: entity.updatedDate ?? ""
+        )
+    }
+    
+    static func bookDomainToEntity(_ domain: BookModel, entity: BookEntity) {
+        entity.id = domain.id
+        entity.author = domain.author
+        entity.coverImage = domain.coverImage
+        entity.price = domain.price
+        entity.publishedDate = domain.publishedDate
+        entity.publisher = domain.publisher
+        entity.stock = domain.stock
+        entity.synopsis = domain.synopsis
+        entity.title = domain.title
+        entity.updatedDate = domain.updatedDate
+    }
+}
