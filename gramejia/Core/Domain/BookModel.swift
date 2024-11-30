3e1353458e8630a96f79058d2904ce16930ac36c
adamilham-dev <adamilham3004@gmail.com>
2024-11-30 22:26:19 +0700

    Create domain model for book

---
+++ gramejia/Core/Domain/BookModel.swift
@@ -0,0 +1,22 @@
+//
+//  BookModel.swift
+//  gramejia
+//
+//  Created by Rivaldo Fernandes on 30/11/24.
+//
+
+import Foundation
+
+struct BookModel: Identifiable {
+    let id: String
+    let author: String
+    let coverImage: String
+    let price: String
+    let publishedDate: String
+    let publisher: String
+    let stock: String
+    let synopsis: String
+    let title: String
+    let updatedDate: String
+    
+}
