2a108d6bf019200ac75bf6efa5d0699459935a75
adamilham-dev <adamilham3004@gmail.com>
2024-12-04 01:19:38 +0700

    Create component snack bar view

---
+++ gramejia/Component/Base/BaseViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewModel.swift
+//  gramejia
+//
+//  Created by Adam on 03/12/24.
+//
+
+import UIKit
+import Combine
+
+class BaseViewModel {
+    var cancellables = Set<AnyCancellable>()
+}
--- gramejia/Component/SnackBar/SnackBarView.swift
+++ gramejia/Component/SnackBar/SnackBarView.swift
@@ -18,17 +18,17 @@ class SnackbarView: UIView {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        setupView()
     }
     
+    override func layoutSubviews() {
+        setCorner(cornerRadius: bounds.size.height / 4)
+    }
     private func setupView() {
         backgroundColor = UIColor.black.withAlphaComponent(0.8)
-        layer.cornerRadius = 8
-        clipsToBounds = true
-        
-        // Configure label
         messageLabel.textColor = .white
-        messageLabel.font = UIFont.systemFont(ofSize: 14)
+        messageLabel.font = .nunitoBold(size: 18)
         messageLabel.textAlignment = .center
         messageLabel.numberOfLines = 0
         
@@ -48,23 +48,19 @@ class SnackbarView: UIView {
     
     func show(in parentView: UIView, duration: TimeInterval = 3.0) {
         parentView.addSubview(self)
-        
-        // Position the snackbar off-screen at the bottom
+       
         translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            leadingAnchor.constraint(equalTo: parentView.leadingAnchor, constant: 16),
-            trailingAnchor.constraint(equalTo: parentView.trailingAnchor, constant: -16),
+            leadingAnchor.constraint(equalTo: parentView.leadingAnchor, constant: 24),
+            trailingAnchor.constraint(equalTo: parentView.trailingAnchor, constant: -24),
             bottomAnchor.constraint(equalTo: parentView.bottomAnchor, constant: 80) // Start off-screen
         ])
         
         parentView.layoutIfNeeded()
-        
-        // Animate the snackbar into view
         UIView.animate(withDuration: 0.3, animations: {
             self.transform = CGAffineTransform(translationX: 0, y: -120) // Move into view
         })
-        
-        // Dismiss after the specified duration
+
         dismissTimer = Timer.scheduledTimer(withTimeInterval: duration, repeats: false) { [weak self] _ in
             self?.dismiss()
         }
@@ -72,10 +68,9 @@ class SnackbarView: UIView {
     
     func dismiss() {
         dismissTimer?.invalidate()
-        
-        // Animate the snackbar out of view
+
         UIView.animate(withDuration: 0.3, animations: {
-            self.transform = .identity // Move back off-screen
+            self.transform = .identity
         }) { _ in
             self.removeFromSuperview()
         }
