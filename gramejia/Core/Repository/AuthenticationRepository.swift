5fd7e49ca75d197b399c2259e78382be64e07a53
adamilham-dev <adamilham3004@gmail.com>
2024-12-04 21:53:47 +0700

    Update Authentication repository for login capabilities

--- gramejia/Core/Repository/AuthenticationRepository.swift
+++ gramejia/Core/Repository/AuthenticationRepository.swift
@@ -10,8 +10,12 @@ import Combine
 
 protocol AuthenticationRepositoryProtocol {
     func registerCustomer(customer: CustomerModel) -> AnyPublisher<Bool, Error>
-    func getCustomer(id: String) -> AnyPublisher<CustomerModel?, Error>
+    func getCustomer(username: String, password: String) -> AnyPublisher<CustomerModel?, Error>
     func getCustomerList() -> AnyPublisher<[CustomerModel], Error>
+    
+    func registerAdmin(admin: AdminModel) -> AnyPublisher<Bool, Error>
+    func getAdmin(username: String, password: String) -> AnyPublisher<AdminModel?, Error>
+    func getAdminList() -> AnyPublisher<[AdminModel]?, Error>
 }
 
 final class AuthenticationRepository: NSObject {
@@ -29,6 +33,24 @@ final class AuthenticationRepository: NSObject {
 }
 
 extension AuthenticationRepository: AuthenticationRepositoryProtocol {
+    func getAdminList() -> AnyPublisher<[AdminModel]?, Error> {
+        return self.coreDataManager.fetch(AdminEntity.self).map { entities in
+            return entities.map { entity in
+                AdminMapper.adminEntityToDomain(entity)
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    func getAdmin(username: String, password: String) -> AnyPublisher<AdminModel?, Error> {
+        let predicate = NSPredicate(format: "username == %@ AND password == %@", username, password)
+        
+        return self.coreDataManager.fetchSingle(AdminEntity.self, predicate: predicate)
+            .map { (entity) -> AdminModel?  in
+                guard let entity: AdminEntity = entity else { return nil }
+                return AdminMapper.adminEntityToDomain(entity)
+            }.eraseToAnyPublisher()
+    }
+    
     func getCustomerList() -> AnyPublisher<[CustomerModel], Error> {
         return self.coreDataManager.fetch(CustomerEntity.self).map { entities in
             return entities.map { entity in
@@ -37,18 +59,26 @@ extension AuthenticationRepository: AuthenticationRepositoryProtocol {
         }.eraseToAnyPublisher()
     }
     
-    func getCustomer(id: String) -> AnyPublisher<CustomerModel?, Error> {
-        let predicate = NSPredicate(format: "id == %@", id)
+    func getCustomer(username: String, password: String) -> AnyPublisher<CustomerModel?, Error> {
+        let predicate = NSPredicate(format: "username == %@ AND password == %@", username, password)
         
-        return self.coreDataManager.fetchSingle(CustomerEntity.self)
+        return self.coreDataManager.fetchSingle(CustomerEntity.self, predicate: predicate)
             .map { (entity) -> CustomerModel?  in
                 guard let entity: CustomerEntity = entity else { return nil }
                 return CustomerMapper.customerEntityToDomain(entity)
             }.eraseToAnyPublisher()
     }
     
+    func registerAdmin(admin: AdminModel) -> AnyPublisher<Bool, Error> {
+        let predicate = NSPredicate(format: "username == %@", admin.username)
+        
+        return self.coreDataManager.addUnique(predicate: predicate) { (entity: AdminEntity) in
+            AdminMapper.adminDomainToEntity(admin, entity: entity)
+        }.eraseToAnyPublisher()
+    }
+    
     func registerCustomer(customer: CustomerModel) -> AnyPublisher<Bool, Error> {
-        let predicate = NSPredicate(format: "id == %@", customer.id)
+        let predicate = NSPredicate(format: "username == %@", customer.username)
         
         return self.coreDataManager.addUnique(predicate: predicate) { (entity: CustomerEntity) in
             CustomerMapper.customeDomainToEntity(customer, entity: entity)
