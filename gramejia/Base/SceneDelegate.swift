97385ec03dd73acf69d99b93b48c9251500b2ab5
adamilham-dev <adamilham3004@gmail.com>
2024-12-21 18:19:29 +0700

    Add condition checking for session in scene delegate

--- gramejia/Base/SceneDelegate.swift
+++ gramejia/Base/SceneDelegate.swift
@@ -16,7 +16,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        // Create the window
+        window = UIWindow(windowScene: windowScene)
+
+        // Check login status
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let isSignedIn: Bool? = UserDefaultsManager.shared.get(forKey: .isSignedIn)
+
+        if isSignedIn == true {
+            let mainVC = storyboard.instantiateViewController(withIdentifier: "MainTabBarController")
+            window?.rootViewController = mainVC
+        } else {
+            let loginVC = storyboard.instantiateViewController(withIdentifier: "MainNavigationController")
+            window?.rootViewController = loginVC
+        }
+
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
